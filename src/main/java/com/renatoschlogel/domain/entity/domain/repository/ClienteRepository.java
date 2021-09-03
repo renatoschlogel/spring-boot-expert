8f1a3af1171364493213fbfea531003b24aec0cd
Renato Welinton Schlogel <renato.s@outlook.com>
2021-09-03 15:32:25 -0300

    querys customizadas e native query

--- src/main/java/com/renatoschlogel/domain/entity/domain/repository/ClienteRepository.java
+++ src/main/java/com/renatoschlogel/domain/entity/domain/repository/ClienteRepository.java
@@ -2,12 +2,22 @@ package com.renatoschlogel.domain.entity.domain.repository;
 
 import com.renatoschlogel.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
     List<Cliente> findByNomeLike(String nome);
+
+    @Query(value = " select c from Cliente c" +
+                   "  where c.nome like :nome")
+    List<Cliente> encontrarPorNome(@Param("nome") String nome);
+
+    @Query(value = " select * from Cliente c" +
+                   "  where c.nome like '%:nome%'", nativeQuery = true)
+    List<Cliente> encontrarPorNomeNativeQuery(@Param("nome") String nome);
 
     boolean existsByNome(String nome);
 }
