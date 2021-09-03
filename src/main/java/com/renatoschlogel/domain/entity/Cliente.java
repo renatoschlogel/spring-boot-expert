a2604d0c4f2013cc616da479bf41d261dc805383
Renato Welinton Schlogel <renato.s@outlook.com>
2021-09-03 00:19:00 -0300

    Mapeando a entidade cliente com anotações JPA

--- src/main/java/com/renatoschlogel/domain/entity/Cliente.java
+++ src/main/java/com/renatoschlogel/domain/entity/Cliente.java
@@ -1,8 +1,17 @@
 package com.renatoschlogel.domain.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String nome;
 
     public Cliente() {
