75adc85b6816fa456580922657bc08a16a483de2
Renato Welinton Schlogel <renato.s@outlook.com>
2021-09-03 20:39:32 -0300

    Metodo GET

--- src/main/java/com/renatoschlogel/VendasApplication.java
+++ src/main/java/com/renatoschlogel/VendasApplication.java
@@ -1,12 +1,24 @@
 package com.renatoschlogel;
 
+import com.renatoschlogel.domain.entity.Cliente;
+import com.renatoschlogel.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
 
-   public static void main(String[] args) {
-        SpringApplication.run(VendasApplication.class, args);
-   }
+    @Bean
+    public CommandLineRunner vai(@Autowired ClienteRepository clienteRepository){
+        return args -> {
+            clienteRepository.save(new Cliente("Renato"));
+        };
+    }
+
+    public static void main(String[] args) {
+         SpringApplication.run(VendasApplication.class, args);
+    }
 }
