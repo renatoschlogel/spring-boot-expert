347cc9bcc17c673fc142aed758a279c7b65417d7
Renato Welinton Schlogel <renato.s@outlook.com>
2021-09-30 23:56:58 -0300

    Ajuste de permissão de acessp ao endpoint do cadastro de usuario

--- src/main/java/com/renatoschlogel/config/SecurityConfig.java
+++ src/main/java/com/renatoschlogel/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.renatoschlogel.config;
 import com.renatoschlogel.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,6 +38,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .hasAnyRole("ADMIN", "USER")
                 .antMatchers("/api/pedidos/**")
                     .hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.POST, "/api/usuarios/**")
+                    .permitAll()
+                .anyRequest().authenticated()
             .and()
                 .httpBasic();
     }
