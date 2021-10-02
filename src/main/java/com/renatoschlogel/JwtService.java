22a128125ef87466e027a79514818a1eb6b8fa50
Renato Welinton Schlogel <renato.s@outlook.com>
2021-10-02 16:39:07 -0300

    Decodificando o token

--- src/main/java/com/renatoschlogel/JwtService.java
+++ src/main/java/com/renatoschlogel/JwtService.java
@@ -1,6 +1,8 @@
 package com.renatoschlogel;
 
 import com.renatoschlogel.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,20 +28,49 @@ public class JwtService {
         long expString = Long.valueOf(expiracao);
         LocalDateTime dataHoraExpericao = LocalDateTime.now().plusMinutes(expString);
 
-        HashMap<String, Object> claims = new HashMap<>();
-
-        claims.put("email", "renato.s@outlook.com");
-        claims.put("roles", "admin");
-
         Date data = Date.from(dataHoraExpericao.atZone(ZoneId.systemDefault()).toInstant());
         return Jwts
                 .builder()
                 .setSubject(usuario.getLogin())
                 .setExpiration(data)
-                .setClaims(claims)
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura )
                 .compact()
                 ;
+    }
+
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    public boolean tokenValido(String token) {
+        try {
+            Claims claims = obterClaims(token);
+            LocalDateTime dataExpiracao = claims.getExpiration().toInstant()
+                                                       .atZone(ZoneId.systemDefault())
+                                                       .toLocalDateTime();
+            return !LocalDateTime.now().isAfter(dataExpiracao);
+        }catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) {
+        return obterClaims(token).getSubject();
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = new SpringApplication().run(VendasApplication.class);
+        JwtService jwtService = run.getBean(JwtService.class);
+        Usuario usuario = Usuario.builder().login("renato").build();
+        String token = jwtService.gerarToken(usuario);
+        System.out.println(token);
+        System.out.println("valido "+ jwtService.tokenValido(token));
+        System.out.println("Login" + jwtService.obterLoginUsuario(token));
     }
 
 }
