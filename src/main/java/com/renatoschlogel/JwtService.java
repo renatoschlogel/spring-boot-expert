b66bcad75b512fef9fa1a485fea77865c26bc4de
Renato Welinton Schlogel <renato.s@outlook.com>
2021-10-02 01:09:13 -0300

    Adicionando mais informações ao token com claims

--- src/main/java/com/renatoschlogel/JwtService.java
+++ src/main/java/com/renatoschlogel/JwtService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
@@ -25,11 +26,17 @@ public class JwtService {
         long expString = Long.valueOf(expiracao);
         LocalDateTime dataHoraExpericao = LocalDateTime.now().plusMinutes(expString);
 
+        HashMap<String, Object> claims = new HashMap<>();
+
+        claims.put("email", "renato.s@outlook.com");
+        claims.put("roles", "admin");
+
         Date data = Date.from(dataHoraExpericao.atZone(ZoneId.systemDefault()).toInstant());
         return Jwts
                 .builder()
                 .setSubject(usuario.getLogin())
                 .setExpiration(data)
+                .setClaims(claims)
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura )
                 .compact()
                 ;
