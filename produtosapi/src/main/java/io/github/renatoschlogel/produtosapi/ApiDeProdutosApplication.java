027784b1c452906751a79cd362bd20f91a21bdad
renato.s@outlook.com.br <rschlogel@hotmail.com>
2025-03-16 23:41:29 -0300

    Estrutura inicial do projeto

---
+++ produtosapi/src/main/java/io/github/renatoschlogel/produtosapi/ApiDeProdutosApplication.java
@@ -0,0 +1,14 @@
+package io.github.renatoschlogel.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeProdutosApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ produtosapi/src/test/java/io/github/renatoschlogel/produtosapi/ApiDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renatoschlogel.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
